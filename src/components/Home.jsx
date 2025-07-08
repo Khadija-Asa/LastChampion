ed37ff2fa4f77bc9c79f9eb6d83c5ed8efe8e0c1
Khadidja <khadija.aitsiali@gmail.com>
2025-07-09 01:00:07 +0200

    done heading for home page + changed margin for header and body + update font

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,29 @@ import Header from './Header';
 const Home = () => {
   return (
     <>
-    <Header/>    
+      <Header/>
+
+      <section className='home_container'>
+
+        <h1 className='flip_container'>
+          <span className="flip_letter">l</span>
+          <span className="flip_letter">a</span>
+          <span className="flip_letter">s</span>
+          <span className="flip_letter">t</span>
+
+          <br />
+
+          <span className="flip_letter">c</span>
+          <span className="flip_letter">h</span>
+          <span className="flip_letter">a</span>
+          <span className="flip_letter">m</span>
+          <span className="flip_letter">p</span>
+          <span className="flip_letter">i</span>
+          <span className="flip_letter">o</span>
+          <span className="flip_letter">n</span>
+        </h1>
+        
+      </section>   
     </>   
   )
 }
