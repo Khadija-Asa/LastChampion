f2a09af02506b3f2dc4a73633200fab48ae8e8f7
Khadidja <khadija.aitsiali@gmail.com>
2026-05-04 11:44:51 +0200

    Updates

--- assets/Heroes-Ce-m2ez9.js
+++
@@ -1,1 +0,0 @@
-import{j as e,h as r}from"./index-DOpOYlWQ.js";import{T as o}from"./Tournament-DOKwWYCq.js";import"./vendor-react-DekamgWB.js";import"./vendor-gsap-CH_iu5NA.js";import"./vendor-icons-BAH7bg4a.js";const h=()=>e.jsx("div",{className:"theme theme_heroes",children:e.jsx(o,{title:"Choose your 8 favorites Super-heroes",data:r})});export{h as default};
