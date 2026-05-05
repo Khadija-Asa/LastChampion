87139a1eca35538de8f1021e1b24f56e2348d5ff
Khadidja <khadija.aitsiali@gmail.com>
2026-05-05 11:12:59 +0200

    Updates

--- assets/Villains-8dfyGxXx.js
+++
@@ -1,1 +0,0 @@
-import{j as i,v as t}from"./index-Vr8Fd0Sa.js";import{T as s}from"./Tournament-4bIzwkuL.js";import"./vendor-react-DekamgWB.js";import"./vendor-gsap-CH_iu5NA.js";import"./vendor-icons-BAH7bg4a.js";const m=()=>i.jsx("div",{className:" theme theme_villains",children:i.jsx(s,{title:"Choose your 8 favorites villains",data:t})});export{m as default};
